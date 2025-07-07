9be7b292199a23e1e37f5b28d91964bf58229dd4
Tshivan <siristim61@gmail.com>
2025-07-07 08:00:05 +0545

    Second commit

---
+++ src/main/java/com/example/git_priactice/GitPriacticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_priactice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPriacticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPriacticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/git_priactice/GitPriacticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_priactice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPriacticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
